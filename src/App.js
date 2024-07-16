078a9958dbdba85ab3200255deab89efbec32ccf
AhmadWMN <ahmadwnabhan2023@gmail.com>
2024-07-16 23:56:05 +0300

    update App.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import ProductDetail from "./pages/ProductDetails/ProductDetails";
